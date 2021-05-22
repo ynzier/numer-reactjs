ba86958ed0f60eb13f3e9c7d657bba19e2c21730
ynzier <80115482+ynzier@users.noreply.github.com>
2021-05-23 03:13:42 +0700

    recheck another

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,8 @@ import {
   render,
 } from '@testing-library/react';
 import App from './App';
-
+window.URL.createObjectURL = function() {};
 test('renders learn react link', () => {
   render( <App /> );
+  
 });
